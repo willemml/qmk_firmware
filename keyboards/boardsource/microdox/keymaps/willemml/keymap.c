63ace06d9f180be4bbc741f0a66c23a165d62f8f
willemml <willem@leit.so>
2021-02-19 16:59:41 -0800

    use qwerty so that i can use software colemak on my macbook

--- keyboards/boardsource/microdox/keymaps/willemml/keymap.c
+++ keyboards/boardsource/microdox/keymaps/willemml/keymap.c
@@ -3,7 +3,7 @@
 #include "g/keymap_combo.h"
 #include "split_util.h"
 
-enum layers { _COLEMAK, _QWERTY, _SPECIALS, _NUMPAD };
+enum layers { _QWERTY, _COLEMAK, _SPECIALS, _NUMPAD };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_split_3x5_3(    KC_Q,    KC_W,          KC_F,          KC_P,          KC_G,        KC_J,         KC_L,    KC_U,           KC_Y,    KC_SCLN,
@@ -13,9 +13,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     ),
 
 	[_QWERTY] = LAYOUT_split_3x5_3(     KC_Q,    KC_W,          KC_E,          KC_R,          KC_T,        KC_Y,         KC_U,    KC_I,           KC_O,    KC_P,
-                                        KC_A,    KC_R,          KC_D,          KC_F,          KC_G,        KC_H,         KC_J,    KC_K,           KC_L,    KC_SCLN,
+                                        KC_A,    KC_S,          KC_D,          KC_F,          KC_G,        KC_H,         KC_J,    KC_K,           KC_L,    KC_SCLN,
                                         KC_Z,    KC_X,          KC_C,          KC_V,          KC_B,        KC_N,         KC_M,    KC_COMM,        KC_DOT,  KC_SLSH,
-                                                                KC_TRNS,       KC_TRNS,       KC_TRNS,     KC_TRNS,      KC_TRNS, KC_TRNS
+                                                                MO(_NUMPAD),   KC_LSFT,       KC_SPC,      KC_BSPC,      KC_LGUI, MO(_SPECIALS)
                                     ),
 
 	[_SPECIALS] = LAYOUT_split_3x5_3(   KC_TRNS, KC_TRNS,       KC_GRV,        KC_QUOT,       KC_TRNS,     TO(_COLEMAK), KC_TRNS, KC_UP,          KC_TRNS, KC_COLN,
